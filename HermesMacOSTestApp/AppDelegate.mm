74e0eb2f9f8acac3b5aba3e165b2a9d5c1266220
Eloy Durán <eloy.de.enige@gmail.com>
2020-04-29 19:39:54 +0200

    First working version with binary distro

---
+++ HermesMacOSTestApp/AppDelegate.mm
@@ -0,0 +1,35 @@
+//
+//  AppDelegate.m
+//  HermesMacOSTestApp
+//
+//  Created by Eloy Durán on 24/04/2020.
+//  Copyright © 2020 Eloy Durán. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+#import <hermes/hermes.h>
+#import <jsi/jsi.h>
+
+using namespace facebook;
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    auto jsiBuffer = std::make_shared<jsi::StringBuffer>("1 + 1");
+    auto runtime = facebook::hermes::makeHermesRuntime();
+    try {
+        auto js = runtime->prepareJavaScript(jsiBuffer, "<eval>");
+        auto result = runtime->evaluatePreparedJavaScript(js);
+        NSLog(@"%s", result.toString(*runtime).utf8(*runtime).c_str());
+    } catch (const jsi::JSIException &e) {
+        NSLog(@"JavaScript terminated via uncaught exception: %s", e.what());
+    }
+}
+
+@end
--- Podfile
+++ Podfile
@@ -2,10 +2,13 @@
 # platform :ios, '9.0'
 
 target 'HermesMacOSTestApp' do
-  platform :osx, "10.14"
+  platform :osx, '10.14'
   # use_frameworks!
 
-  pod 'Hermes/jsi', :path => '/Users/eloy/Code/ReactNative/Hermes/src'
-  pod 'Hermes/dtoa', :path => '/Users/eloy/Code/ReactNative/Hermes/src'
-  pod 'Hermes/LLVHDemangle', :path => '/Users/eloy/Code/ReactNative/Hermes/src'
+  # pod 'Hermes/jsi', path: '/Users/eloy/Code/ReactNative/Hermes/src'
+  # pod 'Hermes/dtoa', path: '/Users/eloy/Code/ReactNative/Hermes/src'
+  # pod 'Hermes/LLVHDemangle', path: '/Users/eloy/Code/ReactNative/Hermes/src'
+  # pod 'Hermes/LLVHSupport', path: '/Users/eloy/Code/ReactNative/Hermes/src'
+
+  pod 'hermes', path: '/Users/eloy/Code/ReactNative/Hermes/build'
 end
