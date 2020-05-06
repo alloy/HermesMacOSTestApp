3e4ff0910c0e20f23dfb4cb79b39332d1f454560
Eloy Durán <eloy.de.enige@gmail.com>
2020-05-06 18:22:42 +0200

    Use dylib podspec

--- HermesMacOSTestApp/AppDelegate.mm
+++ HermesMacOSTestApp/AppDelegate.mm
@@ -21,16 +21,16 @@ using namespace facebook;
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    auto jsiBuffer = std::make_shared<jsi::StringBuffer>("1 + 1");
+//    auto jsiBuffer = std::make_shared<jsi::StringBuffer>("1 + 1");
     auto runtime = facebook::hermes::makeHermesRuntime();
 
-    try {
-        auto js = runtime->prepareJavaScript(jsiBuffer, "<eval>");
-        auto result = runtime->evaluatePreparedJavaScript(js);
-        NSLog(@"%s", result.toString(*runtime).utf8(*runtime).c_str());
-    } catch (const jsi::JSIException &e) {
-        NSLog(@"JavaScript terminated via uncaught exception: %s", e.what());
-    }
+//    try {
+//        auto js = runtime->prepareJavaScript(jsiBuffer, "<eval>");
+//        auto result = runtime->evaluatePreparedJavaScript(js);
+//        NSLog(@"%s", result.toString(*runtime).utf8(*runtime).c_str());
+//    } catch (const jsi::JSIException &e) {
+//        NSLog(@"JavaScript terminated via uncaught exception: %s", e.what());
+//    }
 }
 
 @end
--- Podfile
+++ Podfile
@@ -5,11 +5,5 @@ target 'HermesMacOSTestApp' do
   platform :osx, '10.14'
   # use_frameworks!
 
-  # pod 'Hermes/jsi', path: '/Users/eloy/Code/ReactNative/Hermes/src'
-  # pod 'Hermes/dtoa', path: '/Users/eloy/Code/ReactNative/Hermes/src'
-  # pod 'Hermes/LLVHDemangle', path: '/Users/eloy/Code/ReactNative/Hermes/src'
-  # pod 'Hermes/LLVHSupport', path: '/Users/eloy/Code/ReactNative/Hermes/src'
-
-  # pod 'hermes', path: '/Users/eloy/Code/ReactNative/Hermes/build'
-  pod 'hermes', git: 'https://github.com/alloy/hermes.git', commit: 'd3e41ea5bcdbd920203fb815b49326072e507353'
+  pod 'hermes', git: 'https://github.com/alloy/hermes.git', commit: 'b6053ea8a6dad633516201a735b9f310769d3b32'
 end
